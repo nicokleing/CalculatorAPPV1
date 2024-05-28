1a8bffb937e2b97cece168eca3628801a75522c9
Nicolas Klein <55195457+nicokleing@users.noreply.github.com>
2024-05-28 01:36:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,28 @@ let operator = '';
 function setOperator(op) {
     operator = op;
 }
+
+function calculate() {
+    const num1 = parseFloat(document.getElementById('number1').value);
+    const num2 = parseFloat(document.getElementById('number2').value);
+    let result = 0;
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'Invalid operator';
+    }
+
+    document.getElementById('result').innerText = 'Result: ' + result;
+}
